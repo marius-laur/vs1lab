b6bc1f87b488733f6df66f0e44bb53a87426b36d
Inoyuuuuu <inoyuuuuu@gmail.com>
2025-12-16 00:33:31 +0100

    made initMap into own function, added if to image removal

--- Aufgabe3/gta_v3/public/javascripts/geotagging.js
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -4,6 +4,14 @@
 
 // This script is executed when the browser loads index.html
 
+/* logging copy paste
+    console.log("---------- updated");
+    console.log(tagLatitude.value);
+    console.log(tagLongitude.value);
+    console.log(discoveryLatitude.value);
+    console.log(discoveryLongitude.value);
+*/
+
 /**
  * A function to retrieve the current location and update the page.
  * It is called once the page has been fully loaded.
@@ -15,42 +23,45 @@ function updateLocation() {
     let discoveryLatitude = document.getElementById("discovery-latitude-input");
     let discoveryLongitude = document.getElementById("discovery-longitude-input");
 
-const hasCoordinates = tagLatitude.value !== ""
-        && tagLongitude.value !== "" 
-        && discoveryLatitude.value !== "" 
-        && discoveryLongitude.value !== "";
+    const tagCoordsPresent = tagLatitude.value !== "" && tagLongitude.value !== "";
+    const discoveryCoordsPresent = discoveryLatitude.value !== "" && discoveryLongitude.value !== "";
 
-    if (!hasCoordinates) {
-    
-        console.log("updated");
-        console.log(tagLatitude.value);
-        console.log(tagLongitude.value);
-        console.log(discoveryLatitude.value);
-        console.log(discoveryLongitude.value);
+    if (tagCoordsPresent || discoveryCoordsPresent) {
+        const latitude = tagCoordsPresent ? tagLatitude.value : discoveryLatitude.value;
+        const longitude = tagCoordsPresent ? tagLongitude.value : discoveryLongitude.value;
+        
+        initMap(mapManager, latitude, longitude);
 
-        LocationHelper.findLocation((location) => {
-            tagLatitude.value = location.latitude;
-            tagLongitude.value = location.longitude;
-            discoveryLatitude.value = location.latitude;
-            discoveryLongitude.value = location.longitude;
-
-            mapManager.initMap(location.latitude, location.longitude);
-
-            // Read GeoTags from data-tags
-            let mapDiv = document.getElementById("map");
-            let tagsJson = mapDiv.getAttribute("data-tags");
-            let tagsArray = [];
-            if (tagsJson) {
-                tagsArray = JSON.parse(tagsJson);
-            }
-
-            // Update markers including GeoTags
-            mapManager.updateMarkers(location.latitude, location.longitude, tagsArray);
-
-            document.getElementById("map-image").remove();
-            document.getElementById("map-description").remove();
-        });
+        return;
     }
+
+    LocationHelper.findLocation((location) => {
+        tagLatitude.value = location.latitude;
+        tagLongitude.value = location.longitude;
+        discoveryLatitude.value = location.latitude;
+        discoveryLongitude.value = location.longitude;
+        
+        initMap(mapManager, location.latitude, location.longitude);
+    });
+}
+
+function updateMap(mapManager, latitude, longitude) {
+    mapManager.initMap(latitude, longitude);
+    
+    const mapDiv = document.getElementById("map");
+    let tags = [];
+
+    const tagsJson = mapDiv.getAttribute("data-tags");
+    if (tagsJson) {
+        tags = JSON.parse(tagsJson);
+    }
+
+    mapManager.updateMarkers(latitude, longitude, tags);
+
+    let mapImage = document.getElementById("map-image");
+    let mapDescription = document.getElementById("map-description");
+    if (mapImage) mapImage.remove();
+    if (mapDescription) mapDescription.remove();
 }
 
 // Execute this function automatically after loading the page
