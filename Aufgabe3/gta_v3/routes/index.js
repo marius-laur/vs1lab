7b9ef5703449c785ed190de95d3f1a84920310aa
Inoyu <inoyuuuuu@gmail.com>
2025-12-16 09:45:04 +0100

    try catch

--- Aufgabe3/gta_v3/routes/index.js
+++ Aufgabe3/gta_v3/routes/index.js
@@ -31,7 +31,7 @@ const GeoTag = require('../models/geotag');
 // eslint-disable-next-line no-unused-vars
 const GeoTagExamples = require('../models/geotag-examples');
 const InMemoryGeoTagStore = require('../models/geotag-store');
-const radius = 10;
+const radius = 0.5;
 
 /**
  * Route '/' for HTTP 'GET' requests.
@@ -74,16 +74,21 @@ router.get('/', (req, res) => {
 // TODO: ... your code here ...
 
 router.post('/tagging', (req, res) => {
-  geoTagStore.addGeoTag(new GeoTag(req.body.tagName, req.body.tagLatitude, req.body.tagLongitude, req.body.tagHashtag));
-  let tags = geoTagStore.getNearbyGeoTags(req.body.tagLatitude, req.body.tagLongitude, radius);
   
-  res.render('index', {
-    taglist: tags,
-    tagLat: req.body.tagLatitude,
-    tagLong: req.body.tagLongitude,
-    disLat: req.body.discoveryLatitude,
-    disLong: req.body.discoveryLongitude
-  })
+  try {
+    geoTagStore.addGeoTag(new GeoTag(req.body.tagName, req.body.tagLatitude, req.body.tagLongitude, req.body.tagHashtag));
+    let tags = geoTagStore.getNearbyGeoTags(req.body.tagLatitude, req.body.tagLongitude, radius);
+
+    res.render('index', {
+      taglist: tags,
+      tagLat: req.body.tagLatitude,
+      tagLong: req.body.tagLongitude,
+      disLat: req.body.discoveryLatitude,
+      disLong: req.body.discoveryLongitude
+    })
+  } catch (err) {
+    console.error(err.message());
+  }
 });
 
 /**
@@ -105,15 +110,19 @@ router.post('/tagging', (req, res) => {
 // TODO: ... your code here ...
 
 router.post('/discovery', (req, res) => {
-  let tags = geoTagStore.searchNearbyGeoTags(req.body.discoveryLatitude, req.body.discoveryLongitude, radius, req.body.discoverySearch);
-  
-  res.render('index', {
-    taglist: tags,
-    tagLat: req.body.tagLatitude,
-    tagLong: req.body.tagLongitude,
-    disLat: req.body.discoveryLatitude,
-    disLong: req.body.discoveryLongitude
-  })
+  try {
+    let tags = geoTagStore.searchNearbyGeoTags(req.body.discoveryLatitude, req.body.discoveryLongitude, radius, req.body.discoverySearch);
+    
+    res.render('index', {
+      taglist: tags,
+      tagLat: req.body.tagLatitude,
+      tagLong: req.body.tagLongitude,
+      disLat: req.body.discoveryLatitude,
+      disLong: req.body.discoveryLongitude
+    })
+  } catch (err) {
+    console.error(err.message());
+  }
 });
 
 module.exports = router;
